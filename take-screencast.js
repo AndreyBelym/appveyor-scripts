374cd7bc7d7399fbfa8f311a90625ba361596f0d
Andrey Belym <belym.a.2105@gmail.com>
2017-12-19 18:39:59 +0300

    Update take-screencast.js

--- take-screencast.js
+++ take-screencast.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var child_process = require('child_process');
 
-var ffmpeg = require('child_process').spawn('ffmpeg -f gdigrab -framerate 30 -i desktop -y screencast.mkv', { shell: true });
+var ffmpeg = require('child_process').spawn('ffmpeg -f gdigrab -framerate 30 -i desktop -c:v libx265 -r 30 -preset ultrafast -tune zerolatency -crf 28 -pix_fmt yuv420p -y screencast.mp4', { shell: true });
 
 fs.writeFile('screencast.lock', '', () => {
         const check = () => fs.stat('screencast.lock', (err) => err ? ffmpeg.stdin.write('q\n') : setTimeout(check, 1000));
